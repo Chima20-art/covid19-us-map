dc90a0b3d4a92cc5dc42bcd64cba9e0c527f1b92
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-04-14 01:08:41 +0300

    US API States imported

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Map from "react-usa-map";
 
+import usStates from "./usStates.json";
+import { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [rawData, setRawData] = useState(null);
+
+  useEffect(() => {
+    const data = [];
+    usStates.forEach((item) => {
+      data.push({
+        name: item.abbreviation,
+        cases: Math.floor(Math.random() * 6000),
+      });
+    });
+    setRawData(data);
+    console.log(data);
+    setLoading(false);
+
+    return () => {};
+  }, [loading]);
+
+  const mapHandler = (event) => {
+    alert(event.target.dataset.name);
+  };
+
+  const statesCustomConfig = () => {
+    var config = {};
+    usStates.forEach((item) => {
+      config[item.abbreviation] = {
+        fill: "black",
+        clickHandler: (event) => {
+          alert(item.name);
+        },
+      };
+    });
+
+    return config;
+  };
+
+  if (loading) {
+    return (
+      <div className="App">
+        <h1>Loading...</h1>
+      </div>
+    );
+  } else if (error !== false) {
+    return (
+      <div className="App">
+        <h1>{error.toString()}</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map customize={statesCustomConfig()} onClick={mapHandler} />
     </div>
   );
 }
